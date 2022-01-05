193765e245b8d01a64dbdd39f1f15ccac85a8583
가로 <49224480+Naksang@users.noreply.github.com>
2022-01-06 02:14:46 +0900

    UnityPopol10

---
+++ 21_12_29_Shooting/Assets/Script/Boss1Create.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1Create : MonoBehaviour
+{
+    float _createTime = 8.0f;
+    float _initTime;
+
+    public Transform[] _createPos;
+
+    public GameObject _enemySource;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ 21_12_29_Shooting/Assets/Script/EnemyCreateManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCreateManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
